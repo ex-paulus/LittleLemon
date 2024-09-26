516e43667ecdb96a147d2bc346ac7e98acb2982c
Pavlo Osokin <expaulus@gmail.com>
2024-09-26 18:20:24 +0300

    added booking view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,21 @@
+from .models import *
+from .serializers import *
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+   queryset = Booking.objects.all()
+   serializer_class = BookingSerializer
+   #permission_classes = [permissions.IsAuthenticated]    
 
 def index(request):
     return render(request, 'index.html', {})
