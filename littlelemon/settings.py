7202b8e17ce744456a998d05c98ab9c138bb49cd
Pavlo Osokin <expaulus@gmail.com>
2024-09-26 17:16:25 +0300

    added and migrated models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'LittleLemon', 
+        'USER': 'admindjango', 
+        'PASSWORD': 'employee@123!', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
+        'AUTH_PLUGIN':'mysql_native_password',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
 }
 
 # Password validation
